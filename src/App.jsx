d6e9984a8c39d41d105cd1d1751e7f4fd90886d7
Althea Hanson <altheacodes@gmail.com>
2022-08-24 15:28:50 -0400

    added basic carousel component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
 import './App.css';
+import Carousel from '../components/Carousel' ;
 
-export default function App() {}
+export default function App() {
+
+    return(
+        <div>
+            <Carousel/>
+        </div>
+    )
+
+}
