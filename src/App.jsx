0622cb973cd09c5f7d32dc4d0b43026f2f33d3be
Tanya <spivaktanya93@gmail.com>
2022-08-30 08:59:50 -0300

    Added SocialMediaBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 
 import './App.css';
+import SocialMediaBar from './components/SocialMediaBar'
 import Carousel from './components/Carousel' ;
 import Navbar from './components/NavbarComponent'
 import RecipeCard from './components/RecipeCard';
-
 export default function App() {
 
     return(
         <div>
+            <SocialMediaBar/>
             <Navbar />
             <Carousel/>
             <RecipeCard />
