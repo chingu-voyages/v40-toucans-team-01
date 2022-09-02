10905915aca52d2a64836045cab68c79aaef3da3
RenanMDP <renanmartineli@hotmail.com>
2022-09-02 05:32:04 -0300

    import reader favorites component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
-
 import './App.css';
-import SocialMediaBar from './components/SocialMediaBar'
-import Carousel from './components/Carousel' ;
-import Navbar from './components/NavbarComponent'
+import SocialMediaBar from './components/SocialMediaBar';
+import Carousel from './components/Carousel';
+import Navbar from './components/NavbarComponent';
 import RecipeCard from './components/RecipeCard';
+import ReaderFavorites from './components/ReaderFavorites';
+
 export default function App() {
-
-    return(
-        <div>
-            <SocialMediaBar/>
-            <Navbar />
-            <Carousel/>
-            <RecipeCard />
-        </div>
-    )
-
+  return (
+    <div>
+      <SocialMediaBar />
+      <Navbar />
+      <Carousel />
+      <RecipeCard />
+      <ReaderFavorites />
+    </div>
+  );
 }
-
