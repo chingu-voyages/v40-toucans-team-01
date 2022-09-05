339ab7a5638bf958e1bd24a71c7d6bffeff996e3
KeishaShanice <shanice.lakeisha.sims@gmail.com>
2022-09-05 15:44:05 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import './App.css';
 import SocialMediaBar from './components/SocialMediaBar';
 import Carousel from './components/Carousel';
 import Navbar from './components/NavbarComponent';
-//import RecipeCard from './components/RecipeCard';
 import ReaderFavorites from './components/ReaderFavorites';
 import Footer from './components/Footer';
 
