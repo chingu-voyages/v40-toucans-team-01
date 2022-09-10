09124f18c9c2a5785dd0cdfca03d94f5b27f616b
RenanMDP <renanmartineli@hotmail.com>
2022-09-10 15:38:05 -0300

    wrap components with wrappers

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,24 @@ import SocialMediaBar from './components/SocialMediaBar';
 import Carousel from './components/Carousel';
 import Navbar from './components/NavbarComponent';
 import ReaderFavorites from './components/ReaderFavorites';
+import RecipeCard from './components/RecipeRoundups';
 import FooterSocialMedia from './components/FooterSocialMedia';
 import Footer from './components/Footer';
-
+import Main from './components/Main';
+import MegaWrapper from './components/MegaWrapper';
 
 export default function App() {
   return (
     <div>
       <SocialMediaBar />
       <Navbar />
-      <Carousel />
-      <ReaderFavorites />
+      <Main>
+        <Carousel />
+        <MegaWrapper>
+          <ReaderFavorites />
+          <RecipeCard />
+        </MegaWrapper>
+      </Main>
       <FooterSocialMedia />
       <Footer />
     </div>
