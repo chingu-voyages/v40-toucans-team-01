2f8cddc1d6066a21892754aafa7f246bc9c21a9c
RenanMDP <renanmartineli@hotmail.com>
2022-09-11 14:17:34 -0300

    implement subscribe and pick of the week

---
+++ src/components/Subscribe.jsx
@@ -0,0 +1,177 @@
+export default function Subscribe() {
+  return (
+    <div
+      data-class="subscribe-section-inner"
+      className="xmd:grid xmd:grid-cols-2 xmd:gap-11"
+    >
+      <div
+        data-class="subscribe-box"
+        className="bg-[url('https://minimalistbaker.com/wp-content/themes/mb-2020/assets/images/subscribe-box-background.png')] bg-cover bg-white bg-right mb-10 text-center py-[30px] px-4 md:py-[40px] md:px-[30px] xmd:mb-0 xmd:p-5 mlg:grid mlg:grid-cols-2 mlg:gap-6 mlg:items-center"
+      >
+        <div
+          data-class="subscribe-box__image-container"
+          className="mb-5 max-w-[400px] mx-auto"
+        >
+          <img
+            width="231"
+            height="295"
+            src="https://minimalistbaker.com/wp-content/themes/mb-2020/assets/images/fan-favorites-ebook.png"
+            alt="fan favorites ebook"
+            className="mx-auto"
+          />
+        </div>
+        <div
+          data-class="subscribe-box__content-container"
+          className="max-w-[400px] mx-auto mlg:mx-0"
+        >
+          <h4
+            data-class="subscribe-box__title is-style-alt"
+            className="text-[26px] text-center leading-[1] mb-5 font-bakerOrator tracking-[0.035em] font-normal lowercase xmd:text-[30px]"
+          >
+            Want More Deliciousness?
+          </h4>
+          <p
+            data-class="subscribe-box__content"
+            className="text-center text-[14px] mb-2 break-words"
+          >
+            <strong>
+              Subscribe here and we'll send you an email as new recipes are
+              published.
+            </strong>
+          </p>
+          <p
+            data-class="subscribe-box__content"
+            className="text-center text-[14px] mb-2 break-words"
+          >
+            As a thank you, we'd also love to send you our fan favorites ebook!
+          </p>
+          <div
+            data-class="wpforms-container wpforms-container-full"
+            className="mb-0 mx-auto float-none text-[100%] h-auto tracking-normal static indent-0 drop-shadow-none shadow-none normal-case w-auto visible box-border bg-none border-0 border-none rounded-none list-none outline-0 outline-hidden no-underline overflow-visible m-0 p-0 after:content-[''] after:table after:clear-both"
+          >
+            <form data-class="wpforms-validate wpforms-form">
+              <noscript
+                data-class="wpforms-error-noscript"
+                className="text-[rgb(153, 0, 0)]"
+              >
+                Please enable JavaScript in your browser to complete this form.
+              </noscript>
+              <div
+                data-class="wpforms-field-container"
+                className="float-none text-[100%] h-auto tracking-normal static indent-0 drop-shadow-none shadow-none normal-case w-auto visible box-border bg-none border-0 border-none rounded-none list-none outline-0 outline-hidden no-underline overflow-visible m-0 p-0"
+              >
+                <div
+                  data-class="wpforms-field wpforms-field-email"
+                  className="mt-[18px] mb-4 mx-0 p-0 overflow-x-hidden clear-both sm:overflow-x-visible"
+                >
+                  <label
+                    data-class="wpforms-field-label wpforms-label-hide"
+                    className="hidden absolute top-[-99999px] left-[-99999px] max-w-full font-bold text-[16px] float-none leading-[1.3] break-all mx-0 mt-0 mb-1 p-0 sm:max-w-none"
+                    htmlFor="wpforms-54833-field_1"
+                  >
+                    Email{' '}
+                    <span
+                      data-class="wpforms-required-label"
+                      className="text-[rgb(255, 0, 0)] font-normal float-none text-[100%] h-auto tracking-normal static indent-0 drop-shadow-none shadow-none normal-case w-auto visible box-border bg-none border-0 border-none rounded-none list-none outline-0 outline-hidden no-underline overflow-visible m-0 p-0"
+                    >
+                      *
+                    </span>
+                  </label>
+                  <input
+                    type="email"
+                    id="wpforms-54833-field_1"
+                    name="wpforms[fields][1]"
+                    placeholder="Email Address..."
+                    required
+                    className="bg-[#fcddd5] placeholder:text-[16px] placeholder:font-bakerOrator placeholder:tracking-[0.035em] placeholder:font-normal placeholder:lowercase border border-solid border-[#2a2b2a] max-w-none leading-[1] rounded-none py-6 px-5 placeholder:text-[#333333] box-border block float-none h-[38px] w-full align-middle m-0"
+                  />
+                </div>
+              </div>
+              <div
+                data-class="wpforms-submit-container"
+                className="m-0 p-0 clear-both relative"
+              >
+                <button
+                  type="submit"
+                  name="wpforms[submit]"
+                  id="wpforms-submit-54833"
+                  data-class="wpforms-submit wp-block-button__link"
+                  className="border-none w-full text-[18px] py-[18px] px-5 leading-[1] bg-white font-bakerOrator font-bold uppercase tracking-wide inline-block align-middle h-auto box-border bg-none"
+                >
+                  GET MY EBOOK!
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      <div data-class="social-meter">
+        <div
+          data-class="social-meter__header"
+          className="font-bakerOrator mb-[50px] md:mt-10"
+        >
+          find us elsewhere <strong>@minimalistbaker</strong>
+        </div>
+        <div
+          data-class="social-meter__counts"
+          className="md:grid md:grid-cols-3 md:gap-6"
+        >
+          <a
+            id="instagram"
+            href="#instagram"
+            className="no-underline text-[#2A2B2A] border-b-4 border-solid border-[#D56638] flex justify-between items-end pb-2.5 mb-[30px] font-bold bg-transparent a-transition "
+          >
+            <span
+              data-class="social-meter__count-label"
+              className="font-bakerOrator leading-[1] text-[16px]"
+            >
+              instagram
+            </span>
+            <span
+              data-class="social-meter__count-value"
+              className="text-[30px] font-bold leading-[1] mb-[-3px] xmd:text-[22px] mlg:text-[30px]"
+            >
+              2.1m
+            </span>
+          </a>
+          <a
+            id="pinterest"
+            href="#pinterest"
+            className="no-underline text-[#2A2B2A] border-b-4 border-solid border-[#FFDED6] flex justify-between items-end pb-2.5 mb-[30px] font-bold bg-transparent a-transition "
+          >
+            <span
+              data-class="social-meter__count-label"
+              className="font-bakerOrator leading-[1] text-[16px]"
+            >
+              pinterest
+            </span>
+            <span
+              data-class="social-meter__count-value"
+              className="text-[30px] font-bold leading-[1] mb-[-3px] xmd:text-[22px] mlg:text-[30px]"
+            >
+              381k
+            </span>
+          </a>
+          <a
+            id="facebook"
+            href="#facebook"
+            className="no-underline text-[#2A2B2A] border-b-4 border-solid border-[#D56638] flex justify-between items-end pb-2.5 mb-[30px] font-bold bg-transparent a-transition "
+          >
+            <span
+              data-class="social-meter__count-label"
+              className="font-bakerOrator leading-[1] text-[16px]"
+            >
+              facebook
+            </span>
+            <span
+              data-class="social-meter__count-value"
+              className="text-[30px] font-bold leading-[1] mb-[-3px] xmd:text-[22px] mlg:text-[30px]"
+            >
+              497k
+            </span>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
